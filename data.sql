38b5145873686b53b605ce8e42cf005d5cc1159a
juanpi8345 <juanpipre@outlook.com>
2024-04-14 19:42:30 -0300

    Data

--- data.sql
+++ data.sql
@@ -1,54 +1,52 @@
 USE consultorio_medico;
 
-INSERT INTO consulting_rooms VALUES(
+INSERT INTO consulting_rooms VALUES
 	("1"),
     ("2"),
-    ("3")
-);
+    ("3");
 
-INSERT INTO specialities VALUES (
+
+INSERT INTO specialities VALUES 
 	("Clinica medica"),
     ("Dermatologia"),
     ("Pediatria"),
-    ("Cariologia")
-);
+    ("Cardiologia");
 
-INSERT INTO professionals (professional_dni,end,lastname,name,start) VALUES(
-	("30500000",localtime(2200),"Perez","Leandro",Localtime(0900)),
-    ("22505230",localtime(2000),"Hernandez","Juan",Localtime(1000)),
-    ("15700200",localtime(2200),"Perez","Karina",Localtime(0900)),
-    ("17730200",localtime(2000),"Fernandez","Oscar",Localtime(0800)),
-    ("25530000",localtime(2300),"Pirez","Luciana",Localtime(1100)),
-    ("23000000",localtime(2000),"Ariaga","Juan",Localtime(1000)),
-    ("35300000",localtime(2300),"Gimenez","Jose",Localtime(1100)),
-    ("30520000",localtime(2300),"Ortiz","Marcela",Localtime(1000)),
-	("17730200",localtime(2300),"Martinez","Julian",Localtime(0800)),
-    ("34560000",localtime(2100),"Ruch","Martin",Localtime(0800)),
-    ("21530201",localtime(1900),"Fernandez","Lucia",Localtime(1100))
-);
 
-INSERT INTO professionals_specialities (professional_dni, speciality_name) VALUES(
+INSERT INTO professionals (professional_dni, end, lastname, name, start) VALUES
+    ("30500000", TIME('22:00'), "Perez", "Leandro", TIME('09:00')),
+    ("22505230", TIME('20:00'), "Hernandez", "Juan", TIME('10:00')),
+    ("15700200", TIME('22:00'), "Perez", "Karina", TIME('09:00')),
+    ("17730200", TIME('20:00'), "Fernandez", "Oscar", TIME('08:00')),
+    ("25530000", TIME('23:00'), "Pirez", "Luciana", TIME('11:00')),
+    ("23000000", TIME('20:00'), "Ariaga", "Juan", TIME('10:00')),
+    ("35300000", TIME('23:00'), "Gimenez", "Jose", TIME('11:00')),
+    ("30520000", TIME('23:00'), "Ortiz", "Marcela", TIME('10:00')),
+    ("17730300", TIME('23:00'), "Martinez", "Julian", TIME('08:00')),
+    ("34560000", TIME('21:00'), "Ruch", "Martin", TIME('08:00')),
+    ("21530201", TIME('19:00'), "Fernandez", "Lucia", TIME('11:00'));
+
+
+INSERT INTO professional_specialities (professional_dni, speciality_name) VALUES
 	("30500000","Clinica medica"),
     ("21530201","Clinica medica"),
     ("34560000","Clinica medica"),
     ("25530000","Clinica medica"),
     ("23000000","Dermatologia"),
-	("30520000","Dermatologia"),
+	("15700200","Dermatologia"),
 	("17730200","Dermatologia"),
-	("34560000","Dermatologia"),
-    ("30520000","Pedriatria"),
-    ("35300000","Pedriatria"),
-    ("17730200","Pedriatria"),
-    ("15700200","Pedriatria"),
+	("21530201","Dermatologia"),
+    ("30520000","Pediatria"),
+    ("35300000","Pediatria"),
+    ("17730200","Pediatria"),
+    ("15700200","Pediatria"),
     ("30500000","Cardiologia"),
     ("25530000","Cardiologia"),
     ("30520000","Cardiologia"),
-    ("21530201","Cardiologia")
-);
+    ("21530201","Cardiologia");
 
-INSERT INTO patients (patient_dni,lastname,name) VALUES(
+INSERT INTO patients (patient_dni,lastname,name) VALUES
 	("45199461","Pregliasco","Juan Pablo"),
     ("47200100","Fernandez","Joaquin"),
     ("39200400","Benitez","Lara"),
-    ("40000000","Martinez","Sofia")
-)
+    ("40000000","Martinez","Sofia");
